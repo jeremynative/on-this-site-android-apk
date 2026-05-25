e27fa46f2f734f02e75a5d2586fc4ef26c5c2149
Jeremy Dennis <jeremynative@gmail.com>
2026-05-25 12:54:15 -0400

    Bump Android shell for plant facts grid

--- verify-app-shell.js
+++ verify-app-shell.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 
-const expectedBuild = "20260525-profile-sync-15";
+const expectedBuild = "20260525-plant-grid-facts-16";
 const expectedUrl = "https://nativelongisland.com/archive-test/mobile-app-live.html";
 const mainActivityPath = "app/src/main/java/com/nativelongisland/onthissite/MainActivity.java";
 
