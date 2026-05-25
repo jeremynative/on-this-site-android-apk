86fc2a0bfb7291b57dd6c192af3f29013c3895e6
Jeremy Dennis <jeremynative@gmail.com>
2026-05-25 04:07:01 -0400

    Bump Android shell for account duplicate guard

--- verify-app-shell.js
+++ verify-app-shell.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 
-const expectedBuild = "20260525-plant-review-flow-7";
+const expectedBuild = "20260525-account-duplicate-guard-1";
 const expectedUrl = "https://nativelongisland.com/archive-test/mobile-app-live.html";
 const mainActivityPath = "app/src/main/java/com/nativelongisland/onthissite/MainActivity.java";
 
